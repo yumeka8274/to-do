d12f595e55b8fc8e5c4caa5f557a12dc3bc94c2e
Tattomatsuo <matsuotatto@gmail.com>
2024-05-26 12:09:57 +0800

    my-page
    my-page

---
+++ app/Http/Controllers/MypageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Posts;
+use Illuminate\Support\Facades\Auth;
+
+
+class MypageController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+
+        $user = Auth::user();
+        $posts = Posts::all();
+        // $mypage = MypageController::user();
+        // if($user-> == $mypage){
+        //     return view('posts.mypage',['posts'=>$mypage]);
+        // }    
+        $posts = Posts::where('user_id', $user->id)->get();
+        
+        return view('posts.mypage',['posts'=>$posts]);
+    }
+}
