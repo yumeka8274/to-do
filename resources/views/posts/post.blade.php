6b4c4511c955bf9647f1f7667e8c8d0b991325a5
yumeka8274 <dreamy.8412@gmail.com>
2024-05-28 13:26:53 +0800

    文字を赤にしたよ！！！！

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -63,7 +63,7 @@
         <div class="alert alert-danger">
           <ul>
             @foreach($errors->all() as $error)
-            <li>{{$error}}</li>
+            <li class="text-red-600">{{$error}}</li>
             @endforeach
           </ul>
         </div>
